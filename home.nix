52964157376657e186f2d4d90b74f61f3832ec85
gen740 <54583542+gen740@users.noreply.github.com>
2025-02-12 23:19:58 +0900

    feat(home): Refactoring

--- home.nix
+++ home.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, ... }:
+{ pkgs, ... }:
 {
   home.username = "gen";
   home.homeDirectory = "/Users/gen";
@@ -7,34 +7,28 @@
     ls = "ls --color -F";
     dr = "direnv allow";
     ta = "tmux attach";
-    rebuild-system =
-      if pkgs.stdenv.isDarwin then
-        "darwin-rebuild switch --flake ~/.config/nix-darwin"
-      else if pkgs.stdenv.isLinux then
-        "nixos-rebuild switch"
-      else
-        "echo 'Unsupported platform: ${config.system.platform.system}' && exit 1";
   };
   home.packages = with pkgs; [
-    coreutils-full
     wget
     curl
-    hyperfine
     fswatch
-    btop
-    fd
-    ripgrep
-    gh
     glab
     trash-cli
     nixd
     nixfmt-rfc-style
-    nodejs
     rsync
   ];
+
+  xdg.configFile."git/git-commitmessage.txt" = {
+    source = ./git/git-commitmessage.txt;
+  };
+
   programs = {
     home-manager.enable = true;
     gpg.enable = true;
+    gh.enable = true;
+    ripgrep.enable = true;
+    btop.enable = true;
 
     alacritty = {
       enable = true;
@@ -43,14 +37,24 @@
           ./alacritty/theme.toml
           ./alacritty/keybindings.toml
         ];
-        font.size = 14;
-        font.normal.family = "FiraCode Nerd Font";
+        font.size = 16;
+        font.normal.family = "Cica";
         window = {
           decorations = "none";
           option_as_alt = "Both";
-          startup_mode = "Maximized";
         };
       };
+    };
+
+    fd = {
+      enable = true;
+      ignores = [
+        ".git"
+        "node_modules"
+        ".cache"
+        ".direnv"
+        ".DS_Store"
+      ];
     };
 
     fzf = {
@@ -69,13 +73,6 @@
 
     lazygit = {
       enable = true;
-      settings = {
-        gui = {
-          border = "single";
-          nerdFontsVersion = "3";
-          showBottomLine = false;
-        };
-      };
     };
 
     direnv = {
@@ -86,47 +83,26 @@
     tmux = {
       enable = true;
       escapeTime = 5;
-      baseIndex = 1;
       prefix = "C-s";
       mouse = true;
       shell = pkgs.zsh.outPath + "/bin/zsh";
-      historyLimit = 10000;
       extraConfig = ''
         set-option -g status-justify "centre"
-        set-option -g status-left-length 40
-        set-option -g status-right-length 40
-        set-option -g status-interval 1
-
-        TMUX_STATUS_FG=#cdcecf
-        TMUX_STATUS_DARKFG=#71839b
-        TMUX_STATUS_BG=#393b44
-        TMUX_STATUS_BLUE=#719cd6
-        TMUX_STATUS_BG_BLACK=#192330
-        TMUX_STATUS_GREEN=#81b29a
-
         set-option -g status-style bg=default
-
-        set -g status-left "#[fg=$TMUX_STATUS_BLUE,bold,underscore]#S#[fg=$TMUX_STATUS_GREEN,nobold,nounderscore]       "
-        set -g window-status-format " #[fg=$TMUX_STATUS_BG,bold]#I #[fg=$TMUX_STATUS_BG,nobold]#W"
-        set -g window-status-current-format " #[fg=$TMUX_STATUS_BLUE,bold,underscore]#I #[fg=$TMUX_STATUS_BLUE,nobold]#W"
-        set -g status-right "#[fg=$TMUX_STATUS_BLUE,bold,underscore]%m/%d %H:%M:%S"
-
-        set-option -g status-position bottom
-        set -g pane-border-style fg=color236
-        set -g pane-active-border-style "fg=magenta"
-
+        set -g status-left "[#S]"
+        set -g window-status-format         "#I:#W "
+        set -g window-status-current-format "#[bold]#I:#W*"
+        set -g status-right "#[bold]%H:%M %m-%d"
+        set -g pane-border-style fg=brightblack
+        set -g pane-active-border-style fg=blue
         set-window-option -g mode-keys vi
         bind -T copy-mode-vi v send -X begin-selection
-        bind-key -T copy-mode-vi y send-keys -X copy-pipe-and-cancel "pbcopy"
-
-        bind-key -n C-S-Left swap-window -t -1
-        bind-key -n C-S-Right swap-window -t +1
+        bind-key -T copy-mode-vi y send-keys -X copy-pipe-and-cancel pbcopy
         bind-key k select-pane -U
         bind-key j select-pane -D
         bind-key h select-pane -L
         bind-key l select-pane -R
-
-        set-option -g focus-events on
+        set-option -g focus-events on # send focus events to terminal
       '';
     };
 
@@ -162,26 +138,21 @@
           cmd = ''nvim -d -c "4wincmd w | wincmd J" "$LOCAL" "$BASE" "$REMOTE"  "$MERGED"'';
           keepBackup = false;
         };
-        safe = {
-          directory = "/opt/homebrew";
-        };
-        help = {
-          autocorrect = 20;
-        };
         commit = {
           verbose = true;
-          template = "~/.config/nix-darwin/git/git-commitmessage.txt";
+          template = "~/.config/git/git-commitmessage.txt";
         };
       };
     };
+
     zsh = {
       enable = true;
       initExtra = ''
-        stty stop undef # do not stop the terminal with C-s
-        zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}'
-        PROMPT="%~> ";
-      '';
-      sessionVariables = {
+        stty stop undef
+      ''; # do not stop the terminal with C-s
+      defaultKeymap = "emacs";
+      localVariables = {
+        PROMPT = "%~ ";
         REPORTTIME = 10;
         TIMEFMT = "%*E %*U %*S CPU: %P Memory: %M KB # %J";
         EDITOR = "nvim";
@@ -192,6 +163,9 @@
         XDG_CONFIG_HOME = "$HOME/.config";
         XDG_CACHE_HOME = "$HOME/.cache";
         XDG_DATA_HOME = "$HOME/.local/share";
+      };
+      sessionVariables = {
+        LC_ALL = "en_US.UTF-8";
       };
       history = {
         path = "";
@@ -253,6 +227,7 @@
           yaml
         ]);
       viAlias = true;
+      withNodeJs = true;
     };
   };
 }
