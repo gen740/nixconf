b82da1e6e41124fa52e33a454393b97062a2d8da
gen740 <54583542+gen740@users.noreply.github.com>
2025-05-02 05:26:45 +0900

    feat(nix-darwin): Add system.defaults values

--- nix-darwin/configuration.nix
+++ nix-darwin/configuration.nix
@@ -1,11 +1,17 @@
 {
-  pkgs,
-  lib,
-  inputs,
   ...
 }:
 {
   system = {
     stateVersion = 5;
+    defaults = {
+      NSGlobalDomain = {
+        AppleInterfaceStyle = "Dark";
+        _HIHideMenuBar = true;
+      };
+      dock = {
+        autohide = true;
+      };
+    };
   };
 }
