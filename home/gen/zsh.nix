b3e54acdd22aae0cbccae2f8a5008941b82e078a
gen740 <54583542+gen740@users.noreply.github.com>
2025-05-06 03:06:55 +0900

    fix(zsh): Fix PROMPT

--- home/gen/zsh.nix
+++ home/gen/zsh.nix
@@ -66,7 +66,7 @@
     XDG_CONFIG_HOME = "$HOME/.config";
     XDG_CACHE_HOME = "$HOME/.cache";
     XDG_DATA_HOME = "$HOME/.local/share";
-    PROMPT = "%c ";
+    PROMPT = "%~ ";
   };
   enableCompletion = true;
 }
