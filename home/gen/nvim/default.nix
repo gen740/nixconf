8ba723cacfec21ff8aafaf7409b106babd7d79af
gen740 <54583542+gen740@users.noreply.github.com>
2025-05-07 00:10:16 +0900

    feat(nvim): Delete tree-sitter grammar

--- home/gen/nvim/default.nix
+++ home/gen/nvim/default.nix
@@ -1,41 +1,5 @@
 { pkgs, ... }:
 
-let
-  tree-sitter-with-grammars =
-    langs:
-    let
-      grammars = builtins.listToAttrs (
-        map (lang: {
-          name = lang;
-          value = {
-            grammar = pkgs.tree-sitter-grammars."tree-sitter-${lang}";
-            parser = pkgs.vimPlugins.nvim-treesitter-parsers.${lang};
-          };
-        }) langs
-      );
-
-      names = builtins.attrNames grammars;
-      allInputs = map (n: grammars.${n}.grammar) names ++ map (n: grammars.${n}.parser) names;
-
-      script = builtins.concatStringsSep "\n" (
-        map (
-          lang:
-          let
-            g = grammars.${lang}.grammar;
-            p = grammars.${lang}.parser;
-          in
-          ''
-            mkdir -p $out/parser $out/queries/${lang}
-            ln -s ${g}/parser $out/parser/${lang}.so
-            for f in ${g}/queries/*.scm; do
-              test -e "$f" && ln -s "$f" $out/queries/${lang}/
-            done
-          ''
-        ) names
-      );
-    in
-    pkgs.runCommand "tree-sitter-with-grammars" { nativeBuildInputs = allInputs; } script;
-in
 {
   enable = true;
 
@@ -43,12 +7,6 @@ in
     oil-nvim
     copilot-lua
     github-nvim-theme
-    (tree-sitter-with-grammars [
-      "c"
-      "cpp"
-      "nix"
-      "python"
-    ])
   ];
 
   viAlias = true;
